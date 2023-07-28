c4de6088434b636f4dcecb941fff1441df8f665d
Léo Ansélmo <leonardo_anselmo@hotmail.com>
2023-07-28 08:35:08 -0300

    Exemplos com classList no .js

---
+++ js/script-classList.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('.menu');
+console.log(menu.classList);
+
+menu.classList.add('menu-script');
+menu.classList.remove('menu-script');
+menu.classList.toggle('menu-script');
+menu.classList.toggle('menu-script');
+
+console.log(menu.className);
+
+const animais = document.querySelector('.animais');
+console.log(animais.attributes.class.nodeValue);
+
+const img = document.querySelector('img');
+console.log(img.getAttribute('src'));
+
+img.setAttribute('alt', 'Foto de uma raposa.');
+
+const paragrafo = document.querySelector('p');
+console.log(paragrafo);
+
+ 
